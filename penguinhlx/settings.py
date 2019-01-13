f16ba2618ca7f4e86f802f3fc5739c2049e5748f
pjt <1007307796@qq.com>
2019-01-13 14:08:53 +0800

    weixin qrcode

--- penguinhlx/settings.py
+++ penguinhlx/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -202,3 +199,6 @@ JET_SIDE_MENU_ITEMS = [  # A list of application or custom item dicts
 
     ]},
 ]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
